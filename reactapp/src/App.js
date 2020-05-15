155c884de6e67ffd32c9c7373bca709134ec24e9
Kyle Dahl <kyle.dahl201@gmail.com>
2020-05-14 21:16:32 -0500

    fileMove

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,65 +4,64 @@ import './App.css';
 import axios from 'axios'
 
 
-class App extends React.Component{
+class App extends React.Component {
 
 
 
-constructor(props)
-{super(props)
-  this.state = {
-    employees: [],
-    copyEmployees: []
+  constructor(props) {
+    super(props)
+    this.state = {
+      employees: [],
+      copyEmployees: []
+
+    }
 
   }
 
-}
+  componentDidMount() {
+    axios.get("https://randomuser.me/api/?results=200&nat=us")
+      .then ((results) => {
+        console.log(results.data)
+        this.setState({ employees: results.data.results, copyEmployees: results.data.results })
+      })
 
-componentDidMount(){
-  axios.get("https://randomuser.me/api/?results=200&nat=us")
-  .then(function(results){
-    console.log(results.data)
-    this.setState({employees: results.data, copyEmployees: results.data})
-  })
+  }
 
-}
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.copyEmployees.map(function(emp){
+            return(   <tr>
 
-render(){
-  return (
-    <table className="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-      </tr>
-    </tbody>
-  </table>
+              <td>{emp.name.first}</td>
+              <td>{emp.name.last}</td>
+              <td>{emp.cell}</td>
+              <td>{emp.email}</td>
+            </tr>)
+          })}
+          <tr>
 
-  )
+            <td> </td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
 
-}
+        </tbody>
+      </table>
+
+    )
+
+  }
 
 
 
